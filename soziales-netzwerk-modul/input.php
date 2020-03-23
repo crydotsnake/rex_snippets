598406c92ae9e72fbf548c5d1fb0f0445f67f880
Simon <39345336+crydotsnake@users.noreply.github.com>
2020-03-23 18:39:33 +0100

    Input Code hinzugefügt.

--- soziales-netzwerk-modul/input.php
+++ soziales-netzwerk-modul/input.php
@@ -1,3 +1,29 @@
-<?php 
+<?php
 
-?>
+// base ID
+$id = 2;
+
+// html form
+$form = <<<EOT
+    <fieldset class="form-horizontal ">
+        <legend>Soziales Netzwerk</legend>
+        <div class="form-group">
+            <div class="col-sm-2 control-label"><label for="rv2_1_0_name">Name</label></div>
+            <div class="col-sm-10"><input id="rv2_1_0_name" type="text" name="REX_INPUT_VALUE[$id][0][name]" value="" class="form-control "></div>
+        </div>
+        
+        <div class="form-group">
+            <div class="col-sm-2 control-label"><label for="rv2_1_0_name">Icon Klasse</label></div>
+            <div class="col-sm-10"><input id="rv2_1_0_name" type="text" name="REX_INPUT_VALUE[$id][0][icon]" value="" class="form-control "></div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-sm-2 control-label"><label>URL</label></div>
+            <div class="col-sm-10"><input id="rv2_1_0_name" type="text" name="REX_INPUT_VALUE[$id][0][url]" value="" class="form-control "></div>
+            </div>
+        </div>
+    </fieldset>
+EOT;
+
+// parse form
+echo MBlock::show($id, $form);
