81eac499e42748a9cbfb0f86314ba7cbf8bded0c
Simon <39345336+crydotsnake@users.noreply.github.com>
2020-03-23 18:40:34 +0100

    Output Code hinzugefügt.

--- soziales-netzwerk-modul/output.php
+++ soziales-netzwerk-modul/output.php
@@ -1,3 +1,16 @@
-<?php 
-
+<?php
+$items = rex_var::toArray('REX_VALUE[id=2]'); //wandle modulinput in ein array um
+$out = ''; //$out als leeren string initiieren
+foreach ($items as $item) { //durchlaufe alle eingaben aus dem modulinput
+    $out .= '<li><a target="_blank" href="'.$item['url'].'" class="icon brands '.$item['icon'].'"><span class="label">'.$item['name'].'</span></a></li>';
+    
+}
 ?>
+<nav>
+    <ul>
+        <?php
+            echo $out; //Lis ausgeben
+        ?>
+    </ul>
+</nav>
+
